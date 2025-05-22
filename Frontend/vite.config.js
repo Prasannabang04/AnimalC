589316b79811444b8a15df091bfa2b5de5d21615
Prasannabang04 <prasannabang18@gmail.com>
2025-05-22 18:54:28 +0530

    first commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,       // This makes the server listen on 0.0.0.0
+    port: 5173,       // Optional: ensures consistency with EXPOSE in Dockerfile
+    strictPort: true  // Optional: avoids auto-choosing a different port
+  }
 })
